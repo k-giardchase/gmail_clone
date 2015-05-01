f7af57809c322b484248ae53dc5a240f19dd9b31
Kyle Giard-Chase <kgiardch@gmail.com>
2015-05-01 10:15:58 -0700

    Set up routes.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-angular.module("myApp", [
+var app = angular.module("app", [
   'ngRoute'
 ]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/inbox', {
+      templateUrl: 'views/inbox.html',
+      controller: 'InboxCtrl',
+      controllerAs: 'inbox'
+    })
+    .when('/inbox/email/:id', {
+      templateUrl: 'views/email.html',
+      controller: 'EmailCtrl',
+      controllerAs: 'email'
+    })
+    .otherwise({
+      redirectTo: '/inbox'
+    });
+});
